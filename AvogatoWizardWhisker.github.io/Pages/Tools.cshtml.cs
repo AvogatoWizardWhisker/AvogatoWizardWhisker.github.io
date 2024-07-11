28662ea066586ff241ec870faab98ae4d3de7859
AvogatoWizardWhisker <babis_abdul@hotmail.fr>
2024-07-11 17:20:11 +0100

    Add project files.

---
+++ AvogatoWizardWhisker.github.io/Pages/Tools.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AvogatoWizardWhisker.github.io.Pages
+{
+    public class ToolsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
